456d20016443ac5977b8d75b2d7afbb4e3363836
ypintov <ypinto.vela@gmail.com>
2020-01-12 03:41:44 -0500

    crea todos los componentes de las paginas privadas de la app

---
+++ src/components/pages/Course.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const Courses = () => {
+  return (
+    <div className="ed-grid">
+      <h1>Cursos</h1>
+    </div>
+  )
+}
+
+export default Courses;
---
+++ src/components/pages/Speciality.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Speciality = () => {
+  return (
+    <div className="ed-grid">
+      <h1>Home</h1>
+    </div>
+  )
+}
+export default Speciality;
