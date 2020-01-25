d7207401f094ed95837df131e8003659bfe86be7
ypintov <ypinto.vela@gmail.com>
2020-01-25 11:31:46 -0500

    adecuacion de arguments useEffect

--- src/components/pages/Speciality.jsx
+++ src/components/pages/Speciality.jsx
@@ -7,7 +7,10 @@ import Banner from '../Organisms/Banner';
 
 const Speciality = ({ speciality }) => {
 
-  useEffect(() => store.dispatch(getSpeciality(1), []))
+  useEffect(
+    () => {
+      store.dispatch(getSpeciality(1))
+    }, [])
 
   return (
     <>
